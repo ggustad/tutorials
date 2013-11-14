30573b7c713ba9ec3f68ad30ba3fb7b78d0554da
Jadoro <ggustad@gmail.com>
2013-11-13 21:39:20 -0700

    Initial Commit of the tutorial package

---
+++ Tutuorial/src/GitTest2.java
@@ -0,0 +1,13 @@
+
+public class GitTest2 {
+	public static void main(String[] args) {
+		System.out.println("Git is Cool!");
+		
+		int tools;
+		tools = 0;
+		tools++;
+		
+		System.out.println("Git is cool" + tools + "!!");
+		
+	}
+}
